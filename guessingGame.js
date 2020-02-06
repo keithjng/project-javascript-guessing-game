0fa53ca76e548b6069a3cd84a6c3bf2bd2c3b9e9
Keith Ng <keith.jk.ng@gmail.com>
2020-02-06 14:14:39 -0800

    user input guess

--- guessingGame.js
+++ guessingGame.js
@@ -1,21 +1,40 @@
 let newGuessingGame = require('./lib/newGuessingGame');
 let guessingGameMakeGuess = require('./lib/guessingGameMakeGuess');
 let guessingGameIsDone = require('./lib/guessingGameIsDone');
+let readlineSync = require('readline-sync');
 
-let wordToGuess = 'hello';
+let wordToGuess = 'pterodactyl';
 
-let game = newGuessingGame('hello');
+let game = newGuessingGame('pterodactyl');
 
-console.log(`"Guessing" the word '${wordToGuess}', one letter at a time.`);
-console.log();
+//console.log(`"Guessing" the word '${wordToGuess}', one letter at a time.`);
+//console.log();
 
-for (let letter of wordToGuess) {
+//for (let letter of wordToGuess) {
+
+function amountGuesses() {
+  array = []
+
+  for (let element of wordToGuess) {
+    if (!array.includes(element)) {
+      array.push(element)
+    }
+  }
+}
+
+amountGuesses()
+
+for (let i = 0; i < array.length + 2; i++) {
+  let letter = readlineSync.question('Guess a letter! ');
   console.log(`Guessing letter: ${letter}`);
 
   guessingGameMakeGuess(game, letter);
 
   console.log(`Current word is: ${game.currentWord}`);
   console.log();
+  if (game.currentWord === wordToGuess) {
+    i = array.length
+  }
 }
 
 let isGameDone = guessingGameIsDone(game);
@@ -27,7 +46,7 @@ console.log(`How many turns did we take? ${game.numGuesses}`);
 // The code below is commented out because it will run
 // forever until you correctly imlpement the guessingGameMakeGuess
 // and guessingGameIsDone functions.
-/* let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+let alphabet = 'abcdefghijklmnopqrstuvwxyz';
 
  while (!guessingGameIsDone(game)) {
    for (let letter in alphabet) {
@@ -36,4 +55,3 @@ console.log(`How many turns did we take? ${game.numGuesses}`);
  }
 
  console.log(`Congratulations! You won in ${game.numGuesses} moves`);
- */
